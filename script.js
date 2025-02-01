6a6d89311dd852882ff2f923356b46d31c02cdf9
ishayashika <122111684+ishayashika@users.noreply.github.com>
2025-02-01 15:42:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function volume_sphere() {
     //Write your code here
+	event.preventDefault();
+	let r=parseFloat(document.getElementById("radius").value);
+	if(isNaN(r) || r<=0) {
+		document.getElementById("volume").value="NaN";
+		return;
+	}
+
+	let volume = (4 / 3) * (Math.PI) * Math.pow(r, 3);
+	document.getElementById("volume").value = volume.toFixed(4); 
   
 } 
 
